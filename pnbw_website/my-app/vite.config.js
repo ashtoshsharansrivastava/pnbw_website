cba5d54019a168b64fcad09d367ef5a025e68cda
Ashutosh sharan srivastava <ashutoshsharansrivastava@gmail.com>
2025-08-18 22:20:33 +0530

    changed the vite

--- pnbw_website/my-app/vite.config.js
+++ pnbw_website/my-app/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './', // Add this line to fix the pathing issue.
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: './', // Add this line
+})
